13f4b86b90204387c2627381b10f335d3b7bfc82
JoeCeran <JoeCeran.Joecerankowski@gmail.com>
2019-06-17 08:46:14 -0400

    uploads

--- src/main/java/RPGInheritance/App.java
+++ src/main/java/RPGInheritance/App.java
@@ -16,17 +16,39 @@ public class App {
                 "\nSpeed: " + farmer.getSpeed() +
                 "\nAttack Power: " + farmer.getAttackPower());
 
-        //Create the farmer object
+        farmer.harvest();
+
+        //Create the warrior object
         Warrior warrior = new Warrior();
         warrior.setName("Alice");
 
         //Printout stats
-        System.out.println("\nMy name is " + warrior.getName() + ". I am a farmer. My stats are as listed: " +
+        System.out.println("\nMy name is " + warrior.getName() + ". I am a warrior. My stats are as listed: " +
                 "\nStrength:" + warrior.getStrength() +
                 "\nHealth: " + warrior.getHealth() +
                 "\nStamina: " + warrior.getStamina() +
                 "\nSpeed: " + warrior.getSpeed() +
-                "\nAttack Power: " + warrior.getAttackPower());
+                "\nAttack Power: " + warrior.getAttackPower() +
+                "\nShield Strength Power: " + warrior.getShieldStrength());
+
+        warrior.decreaseHealth();
+        System.out.println("My health is now " + warrior.getHealth());
+
+        //Create the constable object
+        Constable constable = new Constable();
+        constable.setName("Jacob");
+
+        //Printout stats
+        System.out.println("\nMy name is " + constable.getName() + ". I am a constable. My stats are as listed: " +
+                "\nStrength:" + constable.getStrength() +
+                "\nHealth: " + constable.getHealth() +
+                "\nStamina: " + constable.getStamina() +
+                "\nSpeed: " + constable.getSpeed() +
+                "\nAttack Power: " + constable.getAttackPower() +
+                "\nShield Strength Power: " + constable.getJurisdiction());
+
+        constable.increaseStamina();
+        System.out.println("My Stamina is now " + constable.getStamina());
 
     }
 
